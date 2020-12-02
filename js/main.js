5ee16e0ac8d7a26db6d922040e0cbbb57444f5fd
Wonseop Lee <wonseop.s.lee@gmail.com>
2020-12-02 15:12:04 -0800

    listen for submit events on the form

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ var imgSrc = document.querySelector('.avatar-image');
 avatar.addEventListener('input', function (event) {
   imgSrc.setAttribute('src', event.target.value);
 });
+
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+});
