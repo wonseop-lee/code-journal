138f43fa8511e5fc5ef49ee99a787b837342deac
Wonseop Lee <wonseop.s.lee@gmail.com>
2020-12-02 18:02:29 -0800

    form values are assigned into the profile data model

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-var avatar = document.querySelector("input[name='avatar']");
+var avatarInput = document.querySelector("input[name='avatar']");
 var imgSrc = document.querySelector('.avatar-image');
 
-avatar.addEventListener('input', function (event) {
+avatarInput.addEventListener('input', function (event) {
   imgSrc.setAttribute('src', event.target.value);
 });
 
 var form = document.querySelector('form');
 
+var username = document.querySelector("input[name='username']");
+var fullname = document.querySelector("input[name='fullname']");
+var loca = document.querySelector("input[name='location']");
+var bio = document.querySelector('#bio');
+
 form.addEventListener('submit', function (event) {
   event.preventDefault();
+  data.profile.username = username.value;
+  data.profile.fullName = fullname.value;
+  data.profile.location = loca.value;
+  data.profile.avatarUrl = avatarInput.value;
+  data.profile.bio = bio.value;
 });
